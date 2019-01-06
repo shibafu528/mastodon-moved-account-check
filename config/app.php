b5443587b660f139bea5b43e72f27b9511542a5f
shibafu <shibafu528@gmail.com>
2019-01-07 01:25:11 +0900

    だからフォロワーチェックツールじゃねえっつの誤字るな

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'MastodonMovedFollowerCheck'),
+    'name' => env('APP_NAME', 'MastodonMovedAccountCheck'),
 
     /*
     |--------------------------------------------------------------------------
